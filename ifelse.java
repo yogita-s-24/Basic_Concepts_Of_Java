61153f3e443de82ac56e7f4405b9d9e125b10e2b
yogitashete2412 <yogitashete24@gmail.com>
2023-03-01 22:44:04 +0530

    if else statement

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+    public static void main(String args[]) {
+        int a=15;
+        if(a%2==0)
+        {
+            System.out.println("Number is Even");
+        }
+        else
+        {
+            System.out.println("Number is odd");
+        }
+    }
+}
