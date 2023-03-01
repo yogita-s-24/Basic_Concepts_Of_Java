252f160893825cd75a5fed63d77ebf6fd239f03b
yogitashete2412 <yogitashete24@gmail.com>
2023-03-01 22:24:41 +0530

    while loop

---
+++ sec.java
@@ -0,0 +1,19 @@
+//While Loop
+// Syntax-
+  // while(condition)
+  //{
+    // code to be executed
+  //}
+
+
+public class sec {
+    public static void main(String args[]) {
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+}
